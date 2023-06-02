5c6fab0b9533e24af76bd6eb4b3cff2f637eb29b
Juan Castillo <cfuendesign@tutanota.com>
2023-06-02 18:38:01 -0500

    :sparkles: wc + css

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 <img src="https://i.imgur.com/46uy49b.png">
+
+Flora es una librería de Estilos (CSS) y Componentes (Web Components) para construir aplicaciones con un diseño de UI sencillo pero atractivo, igual al de Aula Virtual.
+
+Lo único que necesitas para empezar a hacer uso de Flora es importar el estilo o el componente que quieras.
+
+# Hojas de estilos
+
+### flora.css
+
+La hoja de estilos principal
+
+### flora.icons.css
+
+La hoja de estilos que incluye toda la colección de íconos Phosphor, la cual puedes utilizar mediante la clase `.Icon`
+
+# Componentes
+
+### focusedImage.js
+
+Una imagen focal con un montón de validaciones para hacer su implementación aprueba de tontos
+
+### passwordInput.js
+
+Un campo de contraseña para tus formularios.. O para usarlo solito, como quieras.
--- src/wc/focusedImage.js
+++ src/wc/focusedImage.js
@@ -1,31 +1,77 @@
-import { LitElement, html, unsafeCSS } from "lit";
-import focusedImageStyles from "./focusedImage.css?inline";
-import floraStyles from "../css/flora.css?inline";
+import { LitElement, html, unsafeCSS, css } from "lit";
+// import styles from "../css/flora.css";
+import {
+  propertyIsValidCssSize,
+  propertyUsesValidAspectRatioSyntax,
+  propertyUsesValidBgPositionValue,
+} from "./verifyCSS";
 
 class FocusedImage extends LitElement {
   static get properties() {
     return {
-      concealed: { type: Boolean },
-      width: { type: Number, reflect: true },
-      id: { type: String, reflect: true },
-      name: { type: String, reflect: true },
-      label: { type: String, reflect: true },
+      src: { type: String, reflect: true },
+      alt: { type: String, reflect: true },
+      width: { type: String, reflect: true },
+      aspectratio: { type: String, reflect: true },
+      bgposition: { type: String, reflect: true },
+      displayalt: { type: Boolean, reflect: true },
     };
   }
 
-  static styles = unsafeCSS(floraStyles + "\n" + focusedImageStyles);
+  connectedCallback() {
+    super.connectedCallback()
+    if (!this.alt) {
+      throw Error(
+        "Flora A11yError: Focused Image Web Component must have an alt text"
+      );
+    }
 
-  constructor() {
-    super();
-    this.concealed = true;
-  }
+    if (!this.src) {
+      throw Error(
+        "Flora ComponentError: Focused Image Web Component must have a src"
+      );
+    }
 
-  _concealOrReveal() {
-    this.concealed = !this.concealed;
+    if (this.src) {
+      try {
+        new URL(this.src);
+      } catch (Error) {
+        throw Error(
+          "Flora ComponentError: Focused Image Web Component must have a valid URL as it's src"
+        );
+      }
+    }
+
+    if (this.width) {
+      propertyIsValidCssSize(this.width);
+    }
+
+    if (this.aspectratio) {
+      propertyUsesValidAspectRatioSyntax(this.aspectratio);
+    }
+
+    if (!this.displayalt) {
+      this.displayalt = false;
+    }
+
+    if (!this.backgroundposition) {
+      this.backgroundposition = "center";
+    } else {
+      propertyUsesValidBgPositionValue(this.backgroundposition);
+    }
   }
 
   render() {
     return html`
+      <link rel="stylesheet" href="./src/css/flora.css" />
+      <div
+        class="Img ${this.displayalt ? "" : "Notext"}"
+        style="width: ${this.width}; background-image: url(${this
+          .src}); aspect-ratio: ${this.aspectratio}; background-position: ${this
+          .bgposition}; border-radius: 5px; margin-bottom: 19px;"
+      >
+        <img alt="${this.alt}" />
+      </div>
     `;
   }
 }
